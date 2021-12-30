381cca19a44b0c2abd6f5f2e7afb644d211a836e
shiftleino <>
2021-12-31 00:52:35 +0200

    1.13

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -10,12 +10,19 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
   ]
-
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(0) 
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+  
+  const addVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
 
   return (
     <>
       <div>{anecdotes[selected]}</div>
+      <Button text="vote" handleClick={addVote} />
       <Button text="next anecdote" handleClick={() => setSelected(Math.floor(Math.random()*anecdotes.length))} />
     </>
   )
