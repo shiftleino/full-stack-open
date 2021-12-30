df6f5c57b7d9e0b1fde5e5171ee0a5450d207f60
shiftleino <>
2021-12-30 11:51:58 +0200

    1.4

--- part1/kurssitiedot/src/App.js
+++ part1/kurssitiedot/src/App.js
@@ -2,23 +2,25 @@ import React from 'react'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   return (
     <div>
       <Header course={course}/>
-      <Content parts={[part1, part2, part3]} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
@@ -51,8 +53,9 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
+  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
   return (<>
-    <p>Number of exercises {props.total}</p>
+    <p>Number of exercises {total}</p>
   </>
   )
 }
