0aa9a648562ac1922c0ef741154dbe5334fb9552
shiftleino <>
2022-01-02 11:50:50 +0200

    2.6

---
+++ part2/puhelinluettelo/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import Person from './components/Person'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = { name: newName }
+    setPersons(persons.concat(newPerson))
+    setNewName("")
+  }
+
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input  value={newName} onChange={handlePersonChange}  />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => 
+          <Person key={person.name} name={person.name} />
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
