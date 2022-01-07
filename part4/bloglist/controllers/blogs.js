cd6d12b6d10d98d37d215705a8426c3fb2903327
shiftleino <>
2022-01-07 18:45:45 +0200

    4.10

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -6,13 +6,10 @@ blogsRouter.get("/", async (request, response) => {
     response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post("/", (request, response, next) => {
+blogsRouter.post("/", async (request, response) => {
     const blog = new Blog(request.body)
-    blog.save()
-        .then(result => {
-        response.status(201).json(result)
-        })
-        .catch(error => next(error))
+    const result = await blog.save()
+    response.status(201).json(result)
 })
 
 module.exports = blogsRouter
