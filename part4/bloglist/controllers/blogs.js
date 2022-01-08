461007168019a65fe7edc4ff505caf9fe17cf51f
shiftleino <>
2022-01-08 16:33:33 +0200

    4.20

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require("../models/blog")
 const User = require("../models/user")
 const jwt = require("jsonwebtoken")
 
-const getTokenFrom = request => {
-    const authorization = request.get("authorization")
-    if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-        return authorization.substring(7)
-    }
-    return null
-}
-
 blogsRouter.get("/", async (request, response) => {
     const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 })
     response.json(blogs.map(blog => blog.toJSON()))
@@ -22,8 +14,8 @@ blogsRouter.post("/", async (request, response) => {
     if (!body.title || !body.url) {
         response.status(400).end()
     }
+    const token = request.token
 
-    const token = getTokenFrom(request)
     const decodedToken = jwt.verify(token, process.env.SECRET)
     if (!token || !decodedToken.id) {
         return response.status(401).json({ error: "token missing or invalid" })
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,5 +1,15 @@
 const logger = require("./logger")
 
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get("authorization")
+    if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+        request.token = authorization.substring(7)
+    } else {
+        request.token = null
+    }
+    next()
+}
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: "unknown endpoint" })
 }
@@ -10,12 +20,17 @@ const errorHandler = (error, request, response, next) => {
         return response.status(400).send({ error: "malformatted id" })
     } else if (error.name === "ValidationError") {
         return response.status(400).json({ error: error.message })
+    } else if (error.name === 'JsonWebTokenError') {
+        return response.status(401).json({
+            error: 'invalid token'
+        })
     }
-
+    logger.error(error.message)
     next(error)
 }
 
 module.exports = {
+    tokenExtractor,
     unknownEndpoint, 
     errorHandler
 }
