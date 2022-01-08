e0548f6e049d86abf2fd99e6de1e82ce7844ddfb
shiftleino <>
2022-01-08 13:06:40 +0200

    4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,11 +3,13 @@ require("express-async-errors")
 const app = express()
 const cors = require("cors")
 const blogsRouter  = require("./controllers/blogs")
+const usersRouter = require("./controllers/users")
 const middleware = require("./utils/middleware")
 
 app.use(cors())
 app.use(express.json())
 app.use("/api/blogs", blogsRouter)
+app.use("/api/users", usersRouter)
 app.use(middleware.errorHandler)
 app.use(middleware.unknownEndpoint)
 
--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -11,10 +11,11 @@ mongoose.connect(config.MONGODB_URI)
     })
 
 const blogSchema = mongoose.Schema({
-    title: {type: String, required: true},
+    title: { type: String, required: true },
     author: String,
-    url: {type: String, required: true},
-    likes: Number
+    url: { type: String, required: true },
+    likes: Number,
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
 })
 
 blogSchema.set("toJSON", {
