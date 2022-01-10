5bd38376fd93dc49b95c79604c5900a7870c4a55
shiftleino <>
2022-01-10 15:25:26 +0200

    5.18

--- part5/bloglist/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -8,3 +8,32 @@ describe("Blog app", function() {
         cy.contains("login").click()
     })
 })
+
+describe("Login", function() {
+    beforeEach(function() {
+        const user = {
+            username: "shiftleino",
+            name: "Shiftleino",
+            password: "password"
+        }
+        cy.request("POST", "http://localhost:3003/api/testing/reset")
+        cy.request("POST", "http://localhost:3003/api/users", user)
+        cy.visit("http://localhost:3000")
+    })
+
+    it("succeeds with correct credentials", function() {
+        cy.contains("login").click()
+        cy.get("#username").type("shiftleino")
+        cy.get("#password").type("password")
+        cy.get("#loginButton").click()
+        cy.contains("Shiftleino logged in")
+    })
+
+    it("fails with wrong credentials", function() {
+        cy.contains("login").click()
+        cy.get("#username").type("shiftleino")
+        cy.get("#password").type("wrongpassword")
+        cy.get("#loginButton").click()
+        cy.contains("wrong credentials")
+    })
+})
--- part5/bloglist/bloglist-frontend/src/App.js
+++ part5/bloglist/bloglist-frontend/src/App.js
@@ -148,6 +148,7 @@ const App = () => {
                     <div>
                 username
                         <input
+                            id="username"
                             type="text"
                             value={username}
                             name="Username"
@@ -157,13 +158,14 @@ const App = () => {
                     <div>
             password
                         <input
+                            id="password"
                             type="password"
                             value={password}
                             name="Password"
                             onChange={({ target }) => setPassword(target.value)}
                         />
                     </div>
-                    <button type="submit">login</button>
+                    <button type="submit" id="loginButton" >login</button>
                 </form>
             </div>
         )}
