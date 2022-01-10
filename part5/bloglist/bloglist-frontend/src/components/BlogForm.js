95f1efbed9257a4802a7e43ea35e69612f8f6773
shiftleino <>
2022-01-10 12:03:34 +0200

    5.16

--- part5/bloglist/bloglist-frontend/src/components/BlogForm.js
+++ part5/bloglist/bloglist-frontend/src/components/BlogForm.js
@@ -31,6 +31,7 @@ const BlogForm = ({ createBlog }) => {
                 <div>
                 title:
                     <input
+                        id="title"
                         type="text"
                         value={title}
                         name="Title"
@@ -40,6 +41,7 @@ const BlogForm = ({ createBlog }) => {
                 <div>
                     author:
                     <input
+                        id="author"
                         type="text"
                         value={author}
                         name="Author"
@@ -49,6 +51,7 @@ const BlogForm = ({ createBlog }) => {
                 <div>
                     url:
                     <input
+                        id="url"
                         type="text"
                         value={url}
                         name="Url"
