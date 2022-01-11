d9c548fec8bb9f95354b28f889bf40c90e4f8634
shiftleino <>
2022-01-11 17:42:04 +0200

    6.9&6.10

---
+++ part6/anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,9 @@
+const initialState = "This is a notification"
+
+const reducer = (state= initialState, action) => {
+    switch(action.type) {
+        default: return state
+    }
+}
+
+export default reducer
